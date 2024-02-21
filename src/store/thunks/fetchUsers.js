4bf253747729a6d43ceb2913b04a21a84a3a0420
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-02-21 08:48:53 +0000

    `skeleton loader` added for while loading data

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,17 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
     const response = await axios.get("http://localhost:3005/users");
 
+    // // DEV ONLY!!! DELETE ONCE DONE WITH THE REQUEST
+    // await pause(10000);
+
     return response.data;
 });
+
+// // DEV ONLY!!! DELETE ONCE DONE WITH THE REQUEST
+// const pause = (duration) => {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, duration);
+//     });
+// }
 
 export { fetchUsers };

6163dd99d60c73643200e3c24f9861859ff49e7f
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-02-21 08:48:53 +0000

    `skeleton loader` added for while loading data

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,25 +1,35 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../store';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
-    const dispatch = useDispatch();
-    const { data, isLoading, error } = useSelector((state) => {
-        return state.users
-    })
+  const dispatch = useDispatch();
+  const { data, isLoading, error } = useSelector((state) => {
+    return state.users;
+  });
 
-    useEffect(() => {
-        dispatch(fetchUsers());
-    }, []);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
 
-    if (isLoading) { 
-        return <div>Loading...</div>;
-    }
-    if (error) { 
-        return <div>Error fetching data...</div>;
-    }
+  if (isLoading) {
+    return <Skeleton times={6} className='h-10 w-full' />;
+  }
+  
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
 
-    return <div>{data.length}</div>;
+  const renderedUsers = data.map(user => {
+    return <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+            {user.name}
+        </div>
+    </div>
+  })
+
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
